f4f7315d96da626ef0511eaa00a7698c163d310d
functicons <functicons@gmail.com>
2018-10-21 22:43:59 -0700

    Maven example for shading 3rd party libraries.

---
+++ maven/shade-3rd-party-lib-example/yarn-app/src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.apache.hadoop.yarn.client.api.YarnClient;
+import com.google.common.collect.ImmutableList;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    YarnClient yarnClient = null;
+    ImmutableList immutableList = null;
+    repackaged.com.google.common.collect.ImmutableList repackagedImmutableList = null;
+  }
+}
